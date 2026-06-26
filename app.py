94dd31a9a37dc5fe188b8ffc54056adef4d4c494
padalakomali <padalakomali990@gmail.com>
2026-06-26 23:35:27 +0530

    second

--- app.py
+++ app.py
@@ -53,6 +53,7 @@ app.config['UPLOAD_FOLDER']=UPLOAD_FOLDER
 app.secret_key = "Code123"
 
 app.config["SESSION_COOKIE_SECURE"] = False
+# app.config["SESSION_COOKIE_SECURE"] = False
 app.config["SESSION_COOKIE_HTTPONLY"] = True
 # app.config["SESSION_COOKIE_SAMESITE"] = "Lax"
 app.config["SESSION_COOKIE_SAMESITE"] = "None"
@@ -61,9 +62,9 @@ app.permanent_session_lifetime = timedelta(days=1)
 app.config['PREFERED_URL_SCHEME']='https'
 CORS(
     app,
-    # origins=[
-    #     "https://ecommerce-admin-user-five.vercel.app","http://localhost:5173"
-    # ],
+    origins=[
+        "https://ecommerce-admin-user-five.vercel.app","http://localhost:5173"
+    ],
     supports_credentials=True
 )
 
